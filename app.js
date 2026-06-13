1445a1593a949292817049388c0a58e46faa19c5
sm6303045-prog <sm6303045@gmail.com>
2026-06-13 15:59:17 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import Counter from "./counter.js";
+import Counter2 from "./counter2.js";
+
+export default function App() {
+  return (
+    <div className="App">
+      {/* <Counter /> */}
+      <Counter2 />
+    </div>
+  );
+}
